d515763a77a429c804efab3ea2a4e934e90d2a49
Óscar M. Lage <info@oscarmlage.com>
2018-02-27 21:03:29 +0100

    [FIX] Fixed mess with gitignore

---
+++ pip/task-2/iscowalive/ica.py
@@ -0,0 +1,15 @@
+import requests
+import validators
+
+
+def check(url):
+    try:
+        if(validators.url(url)):
+            r = requests.get(url)
+            if(r.status_code is 200):
+                return True
+            return False
+        else:
+            return False
+    except:
+        return False
